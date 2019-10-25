cbc217572ce50c0b4125be00fbbe6b917bcc8642
Aram Vardanyan <armhash@gmail.com>
2019-10-25 22:18:01 +0300

    add Rectangle project

---
+++ ProRectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ProRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rec = new Rectangle(5 , 5 , 16 , 8 , ConsoleColor.Green);
+            WriteLine(rec);
+            rec.DrawOrClean(true);
+            Thread.Sleep(1500);
+            rec.DrawOrClean(false);
+            rec++;
+            WriteLine(rec);
+            rec.DrawOrClean(true);
+            Thread.Sleep(1500);
+            rec.DrawOrClean(false);
+            WriteLine();
+        }
+    }
+}
---
+++ ProRectangle/Rectangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProRectangle
+{
+    public class Rectangle
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { set; get; }
+        public int Height { set; get; }
+        public ConsoleColor Color { set; get; }
+
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int x, int y, int width, int height, ConsoleColor cl)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Color = cl;
+        }
+        public void DrawOrClean(bool draw)
+        {
+            Console.ForegroundColor = draw ? Color : Console.BackgroundColor;
+
+            Console.SetCursorPosition(X, Y);
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+                Console.CursorLeft = X;
+            }
+
+            Console.ResetColor();
+        }
+        public static Rectangle operator ++(Rectangle rec)
+        {
+            rec.X++;
+
+            return rec;
+        }
+        public void Top()
+        {
+            Y--;
+        }
+        public void Bottom()
+        {
+            Y++;
+        }
+
+        public override string ToString()
+        {
+            return $"{X}\t{Y}\t{Color}";
+        }
+    }
+}
